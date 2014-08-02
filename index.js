a9629e77134e14100d5f89031546c23e77c53302
Nathan Sculli <nathan.sculli@allegiantair.com>
2014-08-02 02:41:23 -0700

    initial load test version.

---
+++ index.js
@@ -0,0 +1,42 @@
+var express = require('express'),
+    app     = express(),
+    xkcd    = require('xkcd-password'),
+    mongo   = require(__dirname + '/lib/mongo')({
+      host: process.env.RPS_HOST,
+      user: process.env.RPS_USER,
+      pass: process.env.RPS_PASS,
+    }),
+    xkcdOpts  = {
+      numWords: 4,
+      minLength: 5,
+      maxLength: 8
+    }
+;
+
+app.use(mongo);
+
+app.get('/', function (req, res) {
+  res.send('hello world');
+});
+
+app.get('/blitz/insert', function (req, res, next) {
+  var collection  = req.mongodb.collection('test_insert_speed'),
+      pw          = new xkcd();
+  //insert a single document.
+  pw.generate(xkcdOpts, function (err, password) {
+    if (err) return next(err);
+
+    var docs  = [
+      { test: (new Date()).getTime(), password: password.join(' ') }
+    ];
+    collection.insert(docs, {}, function (err, result) {
+      if (err) return next(err);
+      res.json(result);
+    });
+  })
+});
+
+var port  = process.env.PORT || 3000
+app.listen(port);
+
+console.log("Server listening on port: "+port);
